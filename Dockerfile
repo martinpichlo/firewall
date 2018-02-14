81e1371b57e186c0ebd988e3923ca5ba4abd5ee5
Zeaq <m dot pichlo at gmx dot de>
2018-02-14 13:24:52 +0100

    remove pathes

--- Dockerfile
+++ Dockerfile
@@ -7,5 +7,5 @@ ADD bin /usr/local/bin
 RUN pacman -Syu --noconfirm iptables iproute2 && \
     chmod u+x /usr/local/bin/*.sh
 
-ENTRYPOINT ["/usr/local/bin/start.sh"]
+ENTRYPOINT ["start.sh"]
 
--- bin/start.sh
+++ bin/start.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 exit_script() {
-    $(/usr/local/bin/iptables_down.sh)
+    $(iptables_down.sh)
 #   trap - SIGINT SIGTERM # clear the trap
     exit 1
 }
 
 trap exit_script SIGINT SIGTERM
 
-$(/usr/local/bin/iptables_up.sh)
-$(/usr/local/bin/iptables_own.sh)
+$(iptables_up.sh)
+$(iptables_own.sh)
 
 #other commands here
 #sleep infinity
